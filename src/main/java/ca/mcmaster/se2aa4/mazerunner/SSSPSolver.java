0c5fa9297ad5208b0b6a14577aae48e100e5581e
davidolejniczak <David.olejniczak@icloud.com>
2024-03-29 22:19:28 -0400

    Skeloton of SSSP class and of methods (close #3)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/SSSPSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/SSSPSolver.java
@@ -1,8 +1,31 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class SSSPSolver implements MazeSolver {
+    private static final Logger logger = LogManager.getLogger();
+    private Maze maze;
     @Override
     public Path solve(Maze maze) {
-        return null;
+        this.maze = maze;
+        Position currPos = maze.getStart();
+        Position prevPos = null;
+        logger.debug("Marking entrance");
+        edgeweightsStorage();
+        edgeweights();
+        return algo();
+    }
+
+    private Path algo() {
+        Path path = new Path();
+        //algo
+        return path;
+    }
+    private void edgeweights() {
+        //algo to assign edge weights
+    }
+    private void edgeweightsStorage() {
+
     }
 }
