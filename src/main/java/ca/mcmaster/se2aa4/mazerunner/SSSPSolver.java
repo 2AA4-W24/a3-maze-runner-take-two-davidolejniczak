a1a4d8941c8f723f8702108d97a08ba3bc72e804
davidolejniczak <David.olejniczak@icloud.com>
2024-03-30 12:28:24 -0400

    Added code to SSSP class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/SSSPSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/SSSPSolver.java
@@ -6,17 +6,18 @@ import org.apache.logging.log4j.Logger;
 public class SSSPSolver implements MazeSolver {
     private static final Logger logger = LogManager.getLogger();
     private Maze maze;
+    private Position currPos;
+    private Position prevPos;
     @Override
     public Path solve(Maze maze) {
         this.maze = maze;
-        Position currPos = maze.getStart();
-        Position prevPos = null;
+        currPos = maze.getStart();
+        prevPos = null;
         logger.debug("Marking entrance");
         edgeweightsStorage();
         edgeweights();
         return algo();
     }
-
     private Path algo() {
         Path path = new Path();
         //algo
@@ -26,6 +27,10 @@ public class SSSPSolver implements MazeSolver {
         //algo to assign edge weights
     }
     private void edgeweightsStorage() {
-
+        int x = maze.getSizeX();
+        int y = maze.getSizeY();
+        int[][] weights  = new int[x][y];
+        weights[currPos.x()][currPos.y()] = 0;
     }
+
 }
